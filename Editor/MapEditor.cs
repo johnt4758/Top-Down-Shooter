129bb4ac010b0c6ab60758954ff9b50f51eef254
johnt4758 <Johnathanmanuel@gmail.com>
2017-04-26 16:26:07 -0700

    Add files via upload

---
+++ Editor/MapEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapEdiort : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator map = target as MapGenerator;
+        if (DrawDefaultInspector()) {
+            map.GenerateMap();
+        }
+
+        if (GUILayout.Button("generate map"))
+        {
+            map.GenerateMap();
+        }
+
+    }
+}
